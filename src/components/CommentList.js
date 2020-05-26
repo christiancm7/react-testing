913b7f8935cbcf95c7031c27cd56726dc55702e4
Christian <atticuschris@yahoo.com>
2020-05-26 18:12:01 -0500

    Higher order component auth created

---
+++ src/components/CommentList.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map((comment) => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Comment List</h4>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+const mapState = (state) => {
+  return { comments: state.comments };
+};
+
+export default connect(mapState)(CommentList);
